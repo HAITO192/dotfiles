ed7db69928d2e0a1e8b4749f26b3b3979102511e
HAITO192 <nguyenthanhhai19022007@gmail.com>
2026-03-23 10:29:20 +0700

    Add .gitignore_global

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@ rm -f ~/.tmux.conf
 
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 
+git config --global core.excludesfile ~/.gitignore_global
 echo 'Cai dat dotfile thanh cong'
