546332518f6f59d30740d18d5354d21d76614f7e
HAITO192 <nguyenthanhhai19022007@gmail.com>
2026-03-27 23:27:10 +0700

    Allow using mouse in Vim

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
-rm -f ~/.bashrc
-rm -f ~/.tmux.conf
-
-ln -s ~/dotfiles/bashrc ~/.bashrc
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/bashrc ~/.bashrc
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/vimrc ~/.vimrc
 
 git config --global core.excludesfile ~/.gitignore_global
 echo 'Cai dat dotfile thanh cong'
